f848822ac735ceffc0cedb68d95d9a7470467798
SHADAB AHMED <134635760+SANDY1980SS@users.noreply.github.com>
2025-09-05 21:44:46 +0530

    Add files via upload

---
+++ Variable.kt
@@ -0,0 +1,17 @@
+fun main(array: Array<String>)
+{
+    var a = 20
+    var b = 20.2
+    var c = "Shadab"
+    println(a)
+    println(c)
+    a = 21 // value can be reassigned is var but not possible in val
+    println(a)
+    println(b)
+    print(a+b)
+    val  d = 14
+    println(d)
+   // d = 15  In val value cannot be reassigned but it is possible in var
+    var number: Int = 10 //Defining Data Type explicitly
+    println(number)
+}
