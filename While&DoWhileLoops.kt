ac6875391eabc818775e1578cc65cb938d4b178d
SHADAB AHMED <134635760+SANDY1980SS@users.noreply.github.com>
2025-09-15 22:08:27 +0530

    Add files via upload

---
+++ While&DoWhileLoops.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+    //while loop
+    var i=1
+    while (i<=10)
+    {
+        println(i)
+        i++
+    }
+
+    //do while loop
+    var j = 1
+    do {
+        println("Shadab")
+        j++
+    }while (j<=5)
+}
